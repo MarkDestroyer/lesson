abc8f79af0fc95b285df814da5f50949ab212866
MarkDestroyer <markkir123@gmail.com>
2021-08-14 02:04:14 +0300

    swift
    add CollectionCustomNewsSize.

---
+++ lesson-1/Cells/Cell Models/FriendsTableViewCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsTableViewCellModel.swift
+//  client-server-1347
+//
+//  Created by Марк Киричко on 14.08.2021.
+//
+
+import Foundation
+
+struct FriendsCellModel {
+    var friendsName: String
+    var cornerShadowView: String
+}
+
